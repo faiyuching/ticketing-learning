7b298de92bdf27a756776449864f9e3d85f9bac9
faiyuching <faiyuching@gmail.com>
2020-11-25 13:18:47 +0800

    lecture 171

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
-import express,{ Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import { validateRequest } from '../middlewares/validate-request'
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user'
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password'
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -13,9 +17,34 @@ router.post('/api/users/signin', [
         .notEmpty()
         .withMessage('You must suppy a password')
 ],
-validateRequest,
-(req: Request, res: Response) => {
+    validateRequest,
+    async (req: Request, res: Response) => {
+        const { email, password } = req.body;
 
-})
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            throw new BadRequestError('Invalid credentials');
+        }
+        const passwordsMatch = await Password.compare(
+            existingUser.password,
+            password
+        );
+        if (!passwordsMatch) {
+            throw new BadRequestError('Invalid credentials');
+        }
+
+        // Generate JWT
+        const userJwt = jwt.sign({
+            id: existingUser.id,
+            email: existingUser.email
+        }, process.env.JWT_KEY!);
+
+        // Store it on session object
+        req.session = {
+            jwt: userJwt
+        };
+
+        res.status(200).send(existingUser);
+    })
 
 export { router as signinRouter };
