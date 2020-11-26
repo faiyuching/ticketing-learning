4d8e6aa44b5f51743af1ef5fcb8f84b783be974a
faiyuching <faiyuching@gmail.com>
2020-11-26 18:54:56 +0800

    lecture 225

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-export default ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps }) => {
     return <Component {...pageProps}/>;
 };
+
+AppComponent.getInitialProps = async appContext => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  console.log(data)
+  return data;
+};
+
+export default AppComponent;
