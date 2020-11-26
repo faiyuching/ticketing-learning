82db2adc501de47a827d3a59f40f8801e3a63d71
faiyuching <faiyuching@gmail.com>
2020-11-26 14:07:26 +0800

    lecture 206

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,19 +1,24 @@
 import { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 export default () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
 
-  const onSubmit = async event => {
+  const onSubmit = async (event) => {
     event.preventDefault();
-    
-    const response = await axios.post('/api/users/signup', {
-      email, password
-    });
 
-    console.log(response.data);
-  }
+    try {
+      const response = await axios.post("/api/users/signup", {
+        email,
+        password,
+      });
+      console.log(response.data);
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
+  };
   return (
     <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
@@ -38,6 +43,16 @@ export default () => {
           className="form-control"
         />
       </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Ooops...</h4>
+          <ul className="my-0">
+            {errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
