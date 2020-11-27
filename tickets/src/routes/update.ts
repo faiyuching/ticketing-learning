47efd89edbe67f0c9e8751567abec0eb2317d9fb
faiyuching <faiyuching@gmail.com>
2020-11-27 20:16:20 +0800

    lecture 263

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -15,6 +15,11 @@ router.put('/api/tickets/:id', requireAuth, async (req: Request, res: Response) 
     if (!ticket) {
         throw new NotFoundError();
     }
+
+    if (ticket.userId !== req.currentUser!.id ) {
+        throw new NotAuthorizedError();
+    }
+
     res.send(ticket);
 })
 
