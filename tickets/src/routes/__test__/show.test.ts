4b05651659944ffc275e339fc56416a07e7a1923
faiyuching <faiyuching@gmail.com>
2020-11-27 19:17:47 +0800

    lecture 259

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,9 +1,11 @@
 import request from 'supertest';
 import { app } from '../../app';
+import mongoose from 'mongoose';
 
 it('reutrns a 404 if the ticket is not found', async () => {
+    const id = new mongoose.Types.ObjectId().toHexString();
     await request(app)
-        .get('/api/tickets/agajksdfhkasj')
+        .get(`/api/tickets/${id}`)
         .send()
         expect(404);
 });
@@ -21,7 +23,7 @@ it('returns the tickets if the ticket is found', async () => {
     const ticketResponse = await request(app)
         .get(`/api/tickets/${reaponse.body.id}`)
         .send()
-        .expect(201);
+        .expect(200);
     
     expect(ticketResponse.body.title).toEqual(title);
     expect(ticketResponse.body.price).toEqual(price);
