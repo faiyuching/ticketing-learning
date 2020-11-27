b724bd6a50b1d2b1f2cf997355b779b2802ed3e0
faiyuching <faiyuching@gmail.com>
2020-11-27 18:07:21 +0800

    lecture 256

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@zhuoming/common';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
@@ -15,8 +16,16 @@ router.post('/api/tickets', requireAuth,
         .withMessage('Price must be greater than 0')
 ], 
 validateRequest, 
-(req: Request, res: Response) => {
-    res.sendStatus(200);
+async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({
+        title,
+        price,
+        userId: req.currentUser!.id
+    })
+    await ticket.save(); 
+    res.status(201).send(ticket);
 })
 
 export { router as createTicketRouter }
