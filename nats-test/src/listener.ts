a174624ebccd52099b1e231170bdea60a79e1312
faiyuching <faiyuching@gmail.com>
2020-11-29 19:42:28 +0800

    lecture 290

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -15,25 +15,7 @@ stan.on('connect', () => {
         process.exit();
     })
 
-    const options = stan.subscriptionOptions()
-        .setManualAckMode(true)
-        .setDeliverAllAvailable()
-        .setDurableName('accounting-service')
-
-    const subscription = stan.subscribe(
-        'ticket:created',
-        'orders-service-queue-group',
-        options
-    )
-
-    subscription.on('message', (msg: Message) => {
-        const data = msg.getData();
-        if (typeof data === 'string') {
-            console.log(`Received event #${msg.getSequence()}, with data: ${data}`)
-        }
-
-        msg.ack();
-    })
+    new TicketCreatedListener(stan).listen();
 })
 
 process.on('SIGINT', () => stan.close());
@@ -83,4 +65,15 @@ abstract class Listener {
             : JSON.parse(data.toString('utf-8'));
     }
     
+}
+
+class TicketCreatedListener extends Listener {
+    subject = 'ticket:created';
+    queueGroupName = 'payments-service';
+
+    onMessage(data: any, msg: Message) {
+        console.log('Event data', data);
+
+        msg.ack();
+    }
 }
