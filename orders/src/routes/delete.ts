8e0a059b7b091de3f96b48de144587ce1dd5265f
faiyuching <faiyuching@gmail.com>
2020-12-01 14:56:41 +0800

    lecture 352

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response } from 'express';
 import { NotAuthorizedError, NotFoundError, requireAuth } from '@zhuoming/common';
 import { Order, OrderStatus } from '../models/order';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper'
 
 const router = express.Router()
 
 router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
     const { orderId } = req.params;
-    const order = await Order.findById(orderId)
+    const order = await Order.findById(orderId).populate('ticket')
     if(!order){
         throw new NotFoundError()
     }
@@ -15,6 +17,14 @@ router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
     }
     order.status = OrderStatus.Cancelled
     await order.save()
+
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+        id: order.id,
+        ticket: {
+            id: order.ticket.id,
+            price: order.ticket.price
+        }
+    })
     
     res.status(204).send(order)
 })
